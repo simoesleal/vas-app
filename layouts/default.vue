cd40115f6fc1829b73b073ad978cc599672d4fd2
Antonio Simões Leal Neto <simoeslealneto@gmail.com>
2021-11-04 22:23:48 -0300

    project commit

---
+++ layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <v-app-bar fixed app :color="'blue'">
+      <v-toolbar-title
+        class="font-weight-medium"
+        :style="'color: white;'"
+        v-text="title"
+      />
+    </v-app-bar>
+    <v-main>
+      <Nuxt />
+    </v-main>
+    <v-footer app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'VAS',
+    }
+  },
+}
+</script>
