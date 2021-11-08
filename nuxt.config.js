aeadb8f375caa5d2092fbbdbf3450a75dcb87700
Antonio Simões Leal Neto <simoeslealneto@gmail.com>
2021-11-08 17:35:21 -0300

    update target server property

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - vas-app',
